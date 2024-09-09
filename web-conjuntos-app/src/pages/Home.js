5388e1b3eefc64684974249ebe3b4a1f67e1243d
EdwardJrS <edward.sierrac@outlook.com>
2024-09-09 18:04:11 -0500

    Configuracion inicial del proyecto y se crear pagina home

---
+++ web-conjuntos-app/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Lista de Conjuntos Residenciales</h1>
+      <p>Aquí aparecerán los conjuntos residenciales disponibles.</p>
+    </div>
+  );
+};
+
+export default Home;
